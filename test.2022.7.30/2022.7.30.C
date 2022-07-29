a3f778b835708d25c4c97c6c92c95fe312f25a36
HZKD513 <544228362@qq.com>
2022-07-29 13:32:59 +0800

    添加项目文件。

---
+++ test.2022.7.30/2022.7.30.C
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//int main()
+//{
+//	int a = 0;
+//	
+//	scanf("%d\n", & a);
+//
+//	printf("转换成天数为 ： %d\n", a * 365);
+//
+//	return 0;
+//}
+
+
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int c = a + b;
+	printf("%d\n", a + b);
+	printf("%d\n", c);
+
+
+
+
+
+	return 0;
+}
